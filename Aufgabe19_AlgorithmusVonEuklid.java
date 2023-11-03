a90639442f93a49735137dace7ede6ad71060828
YayCover9000 <yassuanf@googlemail.com>
2023-11-03 15:11:01 +0100

    ggt dont wokr correctly yet

--- Aufgabe19_AlgorithmusVonEuklid.java
+++ Aufgabe19_AlgorithmusVonEuklid.java
@@ -1,6 +1,22 @@
-
+import java.util.Scanner;
 public class Aufgabe19_AlgorithmusVonEuklid {
 	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int ggt;
+		int zahl1 = sc.nextInt();
+		int zahl2 = sc.nextInt();
+//		int zahl1 = 16;
+//		int zahl2 = 12;
 		
+		if (zahl1%2==0) {
+			if(zahl2%2==0) {
+				ggt = 2;
+				while(zahl1%ggt==0 && zahl2%ggt==0) {
+					ggt = ggt + 2;
+				}
+					System.out.println(ggt-2);
+				
+			}
+		}
 	}
 }
