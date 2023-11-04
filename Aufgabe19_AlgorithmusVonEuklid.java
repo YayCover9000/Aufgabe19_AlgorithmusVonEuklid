285c36e7f895b545982e89652535a19f7568c6ed
YayCover9000 <yassuanf@googlemail.com>
2023-11-04 10:04:51 +0100

    else

--- Aufgabe19_AlgorithmusVonEuklid.java
+++ Aufgabe19_AlgorithmusVonEuklid.java
@@ -3,10 +3,10 @@ public class Aufgabe19_AlgorithmusVonEuklid {
 	public static void main(String [] args) {
 		Scanner sc = new Scanner(System.in);
 		int ggt;
-		int zahl1 = sc.nextInt();
-		int zahl2 = sc.nextInt();
-//		int zahl1 = 16;
-//		int zahl2 = 12;
+//		int zahl1 = sc.nextInt();
+//		int zahl2 = sc.nextInt();
+		int zahl1 = 16;
+		int zahl2 = 12;
 		
 		if (zahl1%2==0) {
 			if(zahl2%2==0) {
